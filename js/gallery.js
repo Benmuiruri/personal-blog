41dad0e2379b2520321c1c00064c677dd651403f
Benmuiruri <ben.muiruri13@gmail.com>
2020-03-18 09:35:15 +0300

    finish the Javascript on Gallery page

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,33 @@
+// Activates the image gallery
+// Attach an event listener to each image in the gallery that loads the image's
+// large photo as the main image
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo > img");
+
+  thumbnails.forEach(function(thumbnail) {
+    //Preload large images.
+    let newImageSrc  = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc
+
+    thumbnail.addEventListener("click", function() {
+      //set clicked image as main image
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", thumbnail.alt);
+
+      // Change which image is current
+      let currentClass = "current";
+      document.querySelector(`.${currentClass}`).classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
